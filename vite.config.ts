d0f0cce718236cd97b0674e9af8d597368e70032
Yatna Artamevia <p087549445@gmail.com>
2025-08-30 15:19:26 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/root/dist/",
   server: {
     host: "::",
     port: 8080,
