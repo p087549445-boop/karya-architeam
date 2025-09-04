748b1cb9ef8d8c8d155de63f5aa6f36bf39bc808
Yatna Artamevia <p087549445@gmail.com>
2025-09-04 13:36:14 +0700

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -96,7 +96,7 @@ const Footer = () => {
           <div className="flex flex-col md:flex-row justify-between items-center">
             <div className="mb-4 md:mb-0">
               <p className="text-white/60 text-sm">
-                © {new Date().getFullYear()} KARYA ARCHITEAM. All rights reserved.By Yatna
+                © {new Date().getFullYear()} KARYA ARCHITEAM. All rights reserved.
               </p>
             </div>
             
