2b36a12294a0227e30846eb2ff530ee061c459fb
Yatna Artamevia <p087549445@gmail.com>
2025-09-06 01:04:12 +0700

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -24,7 +24,7 @@ const HeroSection = () => {
               <span className="block">KARYA</span>
               <span className="block">ARCHITEAM</span>
             </h1>
-            <h2 className="text-base md:text-4xl font-semibold mb-8">
+            <h2 className="text-xl md:text-6xl font-semibold mb-8">
               <span className="block text-gradient">PLAFOND</span>
               <span className="block text-gradient">GYPSUM & PVC</span>
             </h2>
